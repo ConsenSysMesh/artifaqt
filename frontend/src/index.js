79292b25ad79144145a941eec120b925ba372055
jknap <jsknap@hotmail.com>
2018-10-13 17:51:14 +0200

    chore(org): add subdirectories for store and components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,10 @@ import { createStore, compose, applyMiddleware  } from 'redux';
 import { fromJS, Map } from 'immutable';
 import { createLogger } from 'redux-logger';
 
-import reducer from './reducer';
-import initialState from './initialState';
+import reducer from './store/reducer';
+import initialState from './store/initialState';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 let store = createStore(reducer, fromJS(initialState),
   compose(applyMiddleware(createLogger({ stateTransformer: state => Map(state).toJS() }))));

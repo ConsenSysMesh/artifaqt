11559da832e18be75bda73e37f2f4f4a5fca5fe7
jknap <jsknap@hotmail.com>
2018-10-17 21:45:48 +0200

    feat(logging): make logging configurable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware  } from 'redux';
-import { fromJS, Map } from 'immutable';
-import { createLogger } from 'redux-logger';
 
-import reducer from './reducer';
-import initialState from './initialState';
 import './index.css';
-import App from './App';
-
-let store = createStore(reducer, fromJS(initialState),
-  compose(applyMiddleware(createLogger({ stateTransformer: state => Map(state).toJS() }))));
+import App from './components/containers/App';
+import { store } from './store';
 
 ReactDOM.render(
   <Provider store={store}>

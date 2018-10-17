4a3256217101ea50a5b3347af77e794f7c1f1e55
jknap <jsknap@hotmail.com>
2018-10-18 00:12:24 +0200

    refactor(web3): move claiming web3 logic to web3 module

--- frontend/src/web3/index.js
+++ frontend/src/web3/index.js
@@ -5,3 +5,28 @@ import { abi } from './abis/Artifaqt.json';
 
 export const web3 = new Web3(Web3.givenProvider);
 export const Artifaqt = new web3.eth.Contract(abi, contractAddress);
+
+export const claimToken = (data, address, callback) => {
+  const sin = data.sin;
+  const sinHash = web3.utils.sha3(sin);
+  const sinPayloadHash = web3.utils.sha3(sinHash + address.substr(2), { encoding: 'hex' });
+  const sinIndex = data.index;
+
+  console.log(`sinHash = ${sinHash}`);
+  console.log(`sinPayloadHash = ${sinPayloadHash}`);
+
+  Artifaqt.methods.claimToken(
+    sinPayloadHash,
+    sinIndex,
+  ).send({ from: address, gasLimit: 500000, gasPrice: 10**10 })
+      .on('transactionHash',  hash => console.log(`hash = ${hash}`))
+      .on('receipt', receipt => {
+        console.log(receipt)
+        callback();
+      })
+      .on('error', () => {
+        alert(`Something went wrong. 
+        You probably already have this token`)
+        callback()
+      });
+}

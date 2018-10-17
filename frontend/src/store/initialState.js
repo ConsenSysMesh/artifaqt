eca394e97f9d59a50ef08c295397bf6da84b0a06
jknap <jsknap@hotmail.com>
2018-10-17 21:45:13 +0200

    chore(redux): create a directory for redux files

---
+++ frontend/src/store/initialState.js
@@ -0,0 +1,13 @@
+const initialState = {
+  grid: [
+    [1, 2, 3],
+    [4, 0, 5],
+    [6, 7, 8],
+  ],
+  solved: false,
+  canInteract: false,
+  web3: {},
+  tokenIndexes: [],
+}
+
+export default initialState;

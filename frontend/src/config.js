04d3d0b582522ce2d2f9b72ed8341e7d3a6554b3
Kyle <k23burke@gmail.com>
2018-10-27 11:36:50 +0200

    set contract address and network id for prod

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -9,10 +9,11 @@ export const activateLogger = true;
 // export const contractAddress = '0x345ca3e014aaf5dca488057592ee47305d9b3e10'; //Ganache
 // export const contractAddress = '0x25bfc3939528218a362918c7974a932a2a9b9537'; //Ropsten
  // export const contractAddress = '0xfb6a059c20bbf907bcbbddc656a6f831d7f0ae71'; //new Ropseten
- export const contractAddress = '0xee612a9fbe7150e40823e83d7d6ecae16eb340d7'; //Ropsten - feat/no-token-type
+ export const contractAddress = '0x34d565bddcff2dd74bc98e056ebd32dd5f5e1d34'; //Mainnet - feat/no-token-type
  
 /**
  * Ethereum network
  */
-export const requiredNetworkId = '3'; //Ropsten
+export const requiredNetworkId = '1'; //Mainnet
+// export const requiredNetworkId = '3'; //Ropsten
 // export const requiredNetworkId = '5777'; //Ganache
